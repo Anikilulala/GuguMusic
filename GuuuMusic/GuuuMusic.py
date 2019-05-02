3cf68f4e037ddb41560f8ffcc48c3815403d426d
AnikiLulala <loctaa@gmail.com>
2019-05-02 18:16:45 +0800

    up.py finished

--- GuuuMusic/GuuuMusic.py
+++ GuuuMusic/GuuuMusic.py
@@ -1,1 +1,10 @@
-
+from http.server import *
+if __name__ == '__main__':
+    port=8123
+    bind=""
+    protocol="HTTP/1.0"
+    server_address = (bind, port)
+    HandlerClass=CGIHTTPRequestHandler
+    HandlerClass.protocol_version = protocol
+    with HTTPServer(server_address, HandlerClass) as httpd:
+        httpd.serve_forever()
